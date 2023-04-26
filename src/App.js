eda5b2ef0ef8365f9ebd49a533273706cbb8dacd
OMi DHOKATE <42346279+omidhokate2002@users.noreply.github.com>
2023-04-26 13:12:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Weather() {
+  const [inputValue, setInputValue] = useState("");
+  const [data, setData] = useState(null);
+  const [city, setCity] = useState("");
+  const API_KEY = "2fce26b3009e0a66de8c0a0223800869";
+
+  // function to get temp data
+  const getTempData = async (api, query) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=${api}`;
+    try {
+      const response = await axios.get(url);
+      setData(response.data.main);
+      setCity(query);
+    } catch (error) {
+      console.log("error in get data", error);
+      setData(null);
+    }
+  };
+
+  // handle button click
+  const handleButtonClick = () => {
+    getTempData(API_KEY, inputValue);
+  };
+
+  return (
+    <div className="container">
+      <h1 className="text-center my-5">Weather App</h1>
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter City Name"
+              value={inputValue}
+              onInput={(e) => setInputValue(e.target.value)}
+            />
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              onClick={handleButtonClick}
+            >
+              Show Weather
+            </button>
+          </div>
+        </div>
+      </div>
+      {data ? (
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            <div className="card my-5">
+              <div className="card-header">
+                <h5 className="card-title mb-0">
+                  Weather Details of City: {city}
+                </h5>
+              </div>
+              <div className="card-body">
+                <table className="table">
+                  <tbody>
+                    <tr>
+                      <td>Current Temperature:</td>
+                      <td>{data.temp} °C</td>
+                    </tr>
+                    <tr>
+                      <td>Temperature Range:</td>
+                      <td>
+                        {data.temp_min} °C to {data.temp_max} °C
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Humidity:</td>
+                      <td>{data.humidity}</td>
+                    </tr>
+                    <tr>
+                      <td>Sea Level:</td>
+                      <td>{data.sea_level}</td>
+                    </tr>
+                    <tr>
+                      <td>Ground Level:</td>
+                      <td>{data.grnd_level}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+const App = () => {
+  return (
+    <div>
+      <Weather />
+    </div>
+  );
+};
+
+export default App;
